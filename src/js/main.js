4befedba518384584f056b143a472519f67e5b4c
hoangviethung <hoangviet.hung.009@gmail.com>
2020-07-21 17:58:42 +0700

    update

--- src/js/main.js
+++ src/js/main.js
@@ -9,27 +9,41 @@ import AccountController from './lib/AccountController';
 
 // INIT CLASS SUB MENU
 const initClassSubMenu = () => {
-	const items__MainMenu = document.querySelectorAll(
-		'.navbottom__wrapper>.navBar>.navBar__item'
-	);
+	// PARAMS
+	const header = document.querySelector('header');
+	const items__MainMenu = document.querySelectorAll('.navbottom__wrapper>.navBar>.navBar__item');
+	// ADD LOADING HEADER
+	header.setAttribute('loading', '');
+	// INIT FUNCTION
+	const init = () => {
+		return new Promise((resolve, reject) => {
+			items__MainMenu.forEach((item) => {
+				const isHaveSub = item.querySelectorAll('.navBar');
+				// CHECK MAIN MENU IS HAVE SUB ???
+				if (isHaveSub.length > 0) {
+					// ADD CLASS IS HAVE SUB
+					item.classList.add('isHaveSubMenu');
+					// ADD CLASS LIST MENU LV1
+					isHaveSub.forEach((item) => {
+						item.classList.add('navBar--lv1');
+					});
+					// ADD CLASS ITEM MENU LV1
+					const items__MenuLv1 = item.querySelectorAll('.navBar__item');
+					items__MenuLv1.forEach((item) => {
+						item.classList.add('navBar__item--lv1');
+					});
+				}
+			});
+			resolve();
+		})
+	}
 
-	items__MainMenu.forEach((item) => {
-		const isHaveSub = item.querySelectorAll('.navBar');
-		// CHECK MAIN MENU IS HAVE SUB ???
-		if (isHaveSub.length > 0) {
-			// ADD CLASS IS HAVE SUB
-			item.classList.add('isHaveSubMenu');
-			// ADD CLASS LIST MENU LV1
-			isHaveSub.forEach((item) => {
-				item.classList.add('navBar--lv1');
-			});
-			// ADD CLASS ITEM MENU LV1
-			const items__MenuLv1 = item.querySelectorAll('.navBar__item');
-			items__MenuLv1.forEach((item) => {
-				item.classList.add('navBar__item--lv1');
-			});
-		}
-	});
+	init().then(() => {
+		setTimeout(() => {
+			header.removeAttribute('loading');
+			header.setAttribute('loaded', '');
+		}, 300);
+	})
 };
 
 // INIT BUTTON BACK
